ce8949826b73fa1063e7d876ebf9ff437e9db260
Ameer Ameen <ameerapex496@gmail.com>
2023-07-28 14:28:38 +0200

    fixed navbar handler and some file changes

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
         // open menu btn 
 let toggleHandler = document.getElementsByClassName('menubtn')[0]
 let navLinks = document.getElementsByClassName('navlinks')[0]
+let closeMenuBtn = document.getElementsByClassName('close-menubtn')[0]
 
 toggleHandler.addEventListener('click', () =>{
     navLinks.classList.toggle('active');
     toggleHandler.classList.add('deactive');
+    closeMenuBtn.classList.add('active');
 })
 
         // close menu btn 
@@ -12,4 +14,5 @@ let closeMenuHandler = document.getElementsByClassName('close-menubtn')[0]
 closeMenuHandler.addEventListener('click', () => {
     navLinks.classList.toggle('active');
     toggleHandler.classList.remove('deactive');
+    closeMenuBtn.classList.remove('active');
 })
