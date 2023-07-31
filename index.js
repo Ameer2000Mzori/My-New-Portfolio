bb469f0dde29e9f2a2af0ae000b3451d74cf5252
Ameer Ameen <ameerapex496@gmail.com>
2023-07-31 22:48:53 +0200

    fixed all erorrs, linters, and css styles

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 // open burgermenu btn
 const toggleHandler = document.getElementsByClassName("menubtn")[0];
 const navLinks = document.getElementsByClassName("navlinks")[0];
+const closeMenuBtn = document.getElementsByClassName("close-menubtn")[0];
 const body = document.querySelector("body");
 
 toggleHandler.addEventListener("click", () => {
   navLinks.classList.toggle("active");
   toggleHandler.classList.add("deactive");
   body.classList.add("active");
+  closeMenuBtn.classList.add("active");
 });
 
 // close burgermenu btn
@@ -15,6 +17,7 @@ closeMenuHandler.addEventListener("click", () => {
   navLinks.classList.toggle("active");
   toggleHandler.classList.remove("deactive");
   body.classList.remove("active");
+  closeMenuBtn.classList.remove("active");
 });
 
 // open and close btn languages-list menu
