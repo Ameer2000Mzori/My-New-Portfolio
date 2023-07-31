9129fcf8cfa98236d955826238d83fb0d1417a1a
Ameer Ameen <ameerapex496@gmail.com>
2023-07-31 21:25:08 +0200

    updated files in footer

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 // open burgermenu btn
 const toggleHandler = document.getElementsByClassName("menubtn")[0];
 const navLinks = document.getElementsByClassName("navlinks")[0];
+const body = document.querySelector("body");
 
 toggleHandler.addEventListener("click", () => {
   navLinks.classList.toggle("active");
   toggleHandler.classList.add("deactive");
+  body.classList.add("active");
 });
 
 // close burgermenu btn
@@ -12,6 +14,7 @@ const closeMenuHandler = document.getElementsByClassName("close-menubtn")[0];
 closeMenuHandler.addEventListener("click", () => {
   navLinks.classList.toggle("active");
   toggleHandler.classList.remove("deactive");
+  body.classList.remove("active");
 });
 
 // open and close btn languages-list menu
