0b661982cfee87630fd0a166a108851e9196a3a0
Ameer Ameen <ameerapex496@gmail.com>
2023-07-31 17:21:21 +0200

    added javascript linters

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // open burgermenu btn
-let toggleHandler = document.getElementsByClassName("menubtn")[0];
-let navLinks = document.getElementsByClassName("navlinks")[0];
+const toggleHandler = document.getElementsByClassName("menubtn")[0];
+const navLinks = document.getElementsByClassName("navlinks")[0];
 
 toggleHandler.addEventListener("click", () => {
   navLinks.classList.toggle("active");
@@ -8,16 +8,16 @@ toggleHandler.addEventListener("click", () => {
 });
 
 // close burgermenu btn
-let closeMenuHandler = document.getElementsByClassName("close-menubtn")[0];
+const closeMenuHandler = document.getElementsByClassName("close-menubtn")[0];
 closeMenuHandler.addEventListener("click", () => {
   navLinks.classList.toggle("active");
   toggleHandler.classList.remove("deactive");
 });
 
 // open and close btn languages-list menu
-let languagesWarp = document.getElementsByClassName("languages-warp")[0];
-let languagesList = document.getElementsByClassName("languages-list")[0];
-let languagesOnBtn = document.getElementsByClassName("languages-on-btn")[0];
+const languagesWarp = document.getElementsByClassName("languages-warp")[0];
+const languagesList = document.getElementsByClassName("languages-list")[0];
+const languagesOnBtn = document.getElementsByClassName("languages-on-btn")[0];
 languagesWarp.addEventListener("click", () => {
   languagesList.classList.toggle("activated");
   languagesWarp.classList.toggle("activated");
@@ -25,9 +25,9 @@ languagesWarp.addEventListener("click", () => {
 });
 
 // open and close btn languages-list menu
-let frameworksWarp = document.getElementsByClassName("frameworks-warp")[0];
-let frameworksList = document.getElementsByClassName("frameworks-list")[0];
-let frameworksOnBtn = document.getElementsByClassName("frameworks-on-btn")[0];
+const frameworksWarp = document.getElementsByClassName("frameworks-warp")[0];
+const frameworksList = document.getElementsByClassName("frameworks-list")[0];
+const frameworksOnBtn = document.getElementsByClassName("frameworks-on-btn")[0];
 frameworksWarp.addEventListener("click", () => {
   frameworksList.classList.toggle("activated");
   frameworksWarp.classList.toggle("activated");
@@ -35,11 +35,11 @@ frameworksWarp.addEventListener("click", () => {
 });
 
 // open and close btn skills-list menu
-let SkillsWarp = document.getElementsByClassName("Skills-warp")[0];
-let SkillsList = document.getElementsByClassName("Skills-list")[0];
-let SkillsOnBtn = document.getElementsByClassName("Skills-on-btn")[0];
+const SkillsWarp = document.getElementsByClassName("Skills-warp")[0];
+const SkillsList = document.getElementsByClassName("Skills-list")[0];
+const SkillsOnBtn = document.getElementsByClassName("Skills-on-btn")[0];
 SkillsWarp.addEventListener("click", () => {
-    SkillsList.classList.toggle("activated");
+  SkillsList.classList.toggle("activated");
   SkillsWarp.classList.toggle("activated");
   SkillsOnBtn.classList.toggle("activated");
 });
