4a95f946a41a4f75c642e2be2ed0c6aa49d5170f
Ameer Ameen <ameerapex496@gmail.com>
2023-08-03 00:52:17 +0200

    added fonts and fixed padding in skills card

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
-        // open menu btn 
-let toggleHandler = document.getElementsByClassName('menubtn')[0]
-let navLinks = document.getElementsByClassName('navlinks')[0]
+// open burgermenu btn
+const toggleHandler = document.getElementsByClassName("menubtn")[0];
+const navLinks = document.getElementsByClassName("navlinks")[0];
+const closeMenuBtn = document.getElementsByClassName("close-menubtn")[0];
+const body = document.querySelector("body");
 
-toggleHandler.addEventListener('click', () =>{
-    navLinks.classList.toggle('active');
-    toggleHandler.classList.add('deactive');
-})
+toggleHandler.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+  toggleHandler.classList.add("deactive");
+  body.classList.add("active");
+  closeMenuBtn.classList.add("active");
+});
 
-        // close menu btn 
-let closeMenuHandler = document.getElementsByClassName('close-menubtn')[0]
-closeMenuHandler.addEventListener('click', () => {
-    navLinks.classList.toggle('active');
-    toggleHandler.classList.remove('deactive');
-})
+// close burgermenu btn
+const closeMenuHandler = document.getElementsByClassName("close-menubtn")[0];
+closeMenuHandler.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+  toggleHandler.classList.remove("deactive");
+  body.classList.remove("active");
+  closeMenuBtn.classList.remove("active");
+});
