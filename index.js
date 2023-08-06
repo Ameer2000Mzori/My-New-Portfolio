80637b1a53f5423dccd8c1ae2f6a9b7a711df136
Ameer Ameen <ameerapex496@gmail.com>
2023-08-06 14:42:35 +0200

    merged skillsmenu to main

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ toggleHandler.addEventListener("click", () => {
   toggleHandler.classList.add("deactive");
   body.classList.add("active");
   closeMenuBtn.classList.add("active");
-  console.log("clicked me");
 });
 
 // close burgermenu btn
@@ -18,4 +17,34 @@ closeMenuHandler.addEventListener("click", () => {
   toggleHandler.classList.remove("deactive");
   body.classList.remove("active");
   closeMenuBtn.classList.remove("active");
+});
+
+// open and close btn languages-list menu
+const languagesWarp = document.getElementsByClassName("languages-warp")[0];
+const languagesList = document.getElementsByClassName("languages-list")[0];
+const languagesOnBtn = document.getElementsByClassName("languages-on-btn")[0];
+languagesWarp.addEventListener("click", () => {
+  languagesList.classList.toggle("activated");
+  languagesWarp.classList.toggle("activated");
+  languagesOnBtn.classList.toggle("activated");
+});
+
+// open and close btn languages-list menu
+const frameworksWarp = document.getElementsByClassName("frameworks-warp")[0];
+const frameworksList = document.getElementsByClassName("frameworks-list")[0];
+const frameworksOnBtn = document.getElementsByClassName("frameworks-on-btn")[0];
+frameworksWarp.addEventListener("click", () => {
+  frameworksList.classList.toggle("activated");
+  frameworksWarp.classList.toggle("activated");
+  frameworksOnBtn.classList.toggle("activated");
+});
+
+// open and close btn skills-list menu
+const SkillsWarp = document.getElementsByClassName("Skills-warp")[0];
+const SkillsList = document.getElementsByClassName("Skills-list")[0];
+const SkillsOnBtn = document.getElementsByClassName("Skills-on-btn")[0];
+SkillsWarp.addEventListener("click", () => {
+  SkillsList.classList.toggle("activated");
+  SkillsWarp.classList.toggle("activated");
+  SkillsOnBtn.classList.toggle("activated");
 });
